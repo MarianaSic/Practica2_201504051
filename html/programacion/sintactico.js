6debb82fcf7b2362d2751c3069426041ae86f906
MarianaSic <40731091+MarianaSic@users.noreply.github.com>
2020-04-05 10:31:37 -0600

    code 05042020

--- html/programacion/sintactico.js
+++ html/programacion/sintactico.js
@@ -1,3 +1,46 @@
+var i = 0;
+var lista;
+var quebrar = true;
+
 function sintacticoToPython(listaTokens){
-    
+    lista = listaTokens;
+    INICIO();
+}
+
+function INICIO(){
+    LINSTRUCCION();
+}
+
+function LINSTRUCCION(){
+    INSTRUCCION();
+    LINSTRUCCION();
+}
+
+function INSTRUCCION(){
+    if(tkIgual(lista[i].iden, 8)){
+        // es comentario simple
+        console.log(lista[i].valor);
+    }else if(tkIgual(lista[i].iden, 9)){
+        // es comentario multilinea
+        console.log(lista[i].valor);
+    }else if(tkIgual(lista[i].iden, 2)){
+        //son palabras reservadas
+        i--;
+        switch(lista[i].valor){
+            case "int":
+                console.log("es int");
+                break;
+        }
+    }
+}
+
+
+function tkIgual(numero, entrada){
+    if(numero == entrada){
+        if(i < lista.length){
+            i++;
+            return true;
+        }
+    }
+    else return false;
 }
